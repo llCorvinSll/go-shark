93ee88e9770d69c1a5955f44ff8dfa74e37a53b9
Dmitry Pronin <aeoneluminaty2@gmail.com>
2016-08-14 00:22:49 +0300

    little reformat

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	go_shark "./lib"
 	"github.com/google/gopacket/pcap"
 )
@@ -9,29 +10,27 @@ import (
 func main() {
 	fmt.Println("Welcome to go-shark")
 
-	ifaces := go_shark.CaptureInterfaces();
+	ifaces := go_shark.CaptureInterfaces()
 
-	 current_iface := new(pcap.Interface);
+	currentIface := new(pcap.Interface)
 
 	for _, iface := range ifaces {
-		if(go_shark.HasIp(&iface, "192.168.1.101")) {
-			current_iface = &iface;
-			break;
+		if go_shark.HasIp(&iface, "192.168.1.101") {
+			currentIface = &iface
+			break
 		}
 	}
-
 
 	if current_iface != nil {
 		fmt.Println("Interface found")
 
-		go_shark.PrintInterface(*current_iface);
+		go_shark.PrintInterface(*current_iface)
 
-		stream := go_shark.GetPacketsStream(current_iface);
+		stream := go_shark.GetPacketsStream(current_iface)
 
 		for packet := range stream {
-			fmt.Print(packet);
+			fmt.Print(packet)
 		}
-
 
 	}
 }
